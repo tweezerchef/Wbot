0b00558e4391e6f6d12844ccd24afeaac2447397
tweezerchef <ltomblock@gmail.com>
2026-01-21 15:11:26 -0600

    docs: auto-update documentation [skip ci]

--- docs/sidebars.ts
+++ docs/sidebars.ts
@@ -6,7 +6,6 @@ import type { SidebarsConfig } from '@docusaurus/plugin-content-docs';
  */
 const sidebars: SidebarsConfig = {
   docsSidebar: [
-    'intro',
     {
       type: 'category',
       label: 'Architecture',
